171b4b46c376c03871274381a9c3adc405070647
Sure <schurovdaniil@gmail.com>
2024-01-15 17:43:31 +0700

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int input = int.Parse (Console.ReadLine());
+
+if (input % 7 == 0 && input % 23 == 0)
+{
+    Console.WriteLine("Число одновременно кратно 7 и 23");
+}
+else 
+{
+    Console.WriteLine("Число не кратно одновременно 7 и 23");
+}
