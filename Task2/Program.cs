ba7db363af0079dff46b76415a9219bd41036198
Sure <schurovdaniil@gmail.com>
2024-01-15 17:49:08 +0700

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите координаты X отличные от нуля");
+int X = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты Y отличные от нуля");
+int Y = int.Parse(Console.ReadLine());
+if (X > 0 && Y > 0)
+{
+    Console.WriteLine("Вы указали координаты первой четверти");
+}
+else if (X < 0 && Y > 0)
+{
+    Console.WriteLine("Вы указали координаты второй четверти");
+}
+else if (X < 0 && Y < 0)
+{
+    Console.WriteLine("Вы указали координаты третьей четверти");
+}
+else if (X > 0 && Y < 0)
+{
+    Console.WriteLine("Вы указали координаты четвертой четверти");
+}
+else if (X == 0 || Y == 0)
+{
+    Console.WriteLine("Введите координаты отличные от нуля!");
+}
