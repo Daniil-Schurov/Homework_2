4230ee27b65c790fc97586f741a9369f48d304fd
Sure <schurovdaniil@gmail.com>
2024-01-15 18:58:20 +0700

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿int num = new Random().Next(10, 100);
+int num1 = num / 10;
+int num2 = num % 10;
+int max = 0;
+
+if (num1 >= num2)
+{
+    max = num1;
+}
+else
+{
+    max = num2;
+}
+Console.WriteLine("При анализе числа " + num + " Выявлена наибольшая цифра: " + max);
