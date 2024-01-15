4cab6eec98535e21bbfbf490b1caa12281c68945
Sure <schurovdaniil@gmail.com>
2024-01-15 20:27:41 +0700

    Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число N");
+
+string s = Console.ReadLine();
+for (int i = 0; i < s.Length; i++)
+{
+    if (i == s.Length - 1)
+    {
+        Console.Write($"{s[i]}");
+    }
+    else if (i < s.Length - 1)
+    {
+        Console.Write($"{s[i]}, ");
+    }
+}
